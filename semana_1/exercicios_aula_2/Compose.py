a9d07a25175438157b5c8e71682666ae79968c92
Cleyson Diego da Silva <cleyson7@hotmail.com>
2019-03-13 11:41:18 -0300

    Exercicio 16 e exemplo de Compose

---
+++ semana_1/exercicios_aula_2/Compose.py
@@ -0,0 +1,9 @@
+def compose (*funcs):
+    def inner(arg):
+        state = arg
+        for f in reversed(funcs):
+            print(f, state)
+            state = f(state)
+        return state
+    return inner
+
