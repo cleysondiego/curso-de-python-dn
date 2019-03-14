42dc98024661f728b2f7aea22d1e4f03b9e36e6f
Cleyson Diego da Silva <cleyson7@hotmail.com>
2019-03-14 11:59:56 -0300

    Exercicio 17

--- semana_1/exercicios_aula_2/Exercicio17.py
+++ semana_1/exercicios_aula_2/Exercicio17.py
@@ -1,5 +1,6 @@
 '''
-Faça uma função que receba uma função e uma lista de números, aplique essa função a todos os valores da lista e retorne uma nova lista com os valores processados:
+Faça uma função que receba uma função e uma lista de números, 
+aplique essa função a todos os valores da lista e retorne uma nova lista com os valores processados:
 
 aplica(func, list) -> [func(list[0]), func(list[n]), ..]
 aplica(soma_1, [1, 2, 3]) -> [2, 3, 4]
