5c147e181579f758295d1017423b2fdc15d36a1a
Cleyson Diego da Silva <cleyson7@hotmail.com>
2019-04-02 16:36:26 -0300

    #To run: behave (Caminho do .feature) ou python3 -m behave (caminho do .feature)

--- semana_3/exercicios_aula_5/features/jokenpo.feature
+++ semana_3/exercicios_aula_5/features/jokenpo.feature
@@ -5,10 +5,11 @@ Funcionalidade: Jokenpo
     Para decidir quem escolherá o primeiro jogador
 
 Cenário: Capt. A e Capt. B empatam
-    Quando C.A jogadr "papel" e C.B jogar "papel"
+    Quando C.A jogar "papel" e C.B jogar "papel"
     Então gera empate
 
 Cenário: Capt. A ganha de Capt. B
-    Quando C.A jogar "papel" e C.B jogar "pedra"
-    Então "papel" ganha de "pedra"
-    então C.A ganha
+    Quando C.A jogar "Papel" e C.B jogar "Pedra"
+    Então "Papel" ganha
+
+#To run: behave (Caminho do .feature) ou python3 -m behave (caminho do .feature)
--- semana_3/exercicios_aula_5/features/steps/jokenpo.py
+++ semana_3/exercicios_aula_5/features/steps/jokenpo.py
@@ -1,13 +1,53 @@
-from behave import when
+from behave import when, then
+from typing import Text
 
+def jogo_jokenpo(mao_jogador1: Text, mao_jogador2: Text) -> Text:
+    """
+    Jogo do jokenpo
 
+    Regras de entrada: Cada jogador escolhe um elemento válido (pedra, papel, tesoura). O resultado será
+    avaliado com base nestas regras pré-definidas:
+        - Pedra ganha de tesoura;
+        - Tesoura ganha de papel;
+        - Papel ganha de pedra;
+        - Entradas iguais empatam.
+    Caso o elemento não esteja dentro da lista, retorna inválido.
 
+    >>> jogo_jokenpo('tesoura', 'tesoura')
+    'Empate'
 
+    >>> jogo_jokenpo('vassoura', 'papel')
+    'Argumento inválido'
 
-@when('C.A jogadr "{mao_1}" e C.B jogar "{mao_2}"')
+    >>> jogo_jokenpo('Pedra', 'tesoura')
+    'Pedra ganhou'
+
+    """
+    dicionario = {'pedra': 'papel', 'papel': 'tesoura', 'tesoura': 'pedra'}
+
+    mao_jogador1 = mao_jogador1.lower()
+    mao_jogador2 = mao_jogador2.lower()
+    elementos_validos = ['pedra', 'papel', 'tesoura']
+
+    if mao_jogador1 not in elementos_validos or mao_jogador2 not in elementos_validos:
+        return 'Argumento inválido'
+
+    if mao_jogador1 == mao_jogador2:
+        return 'Empate'
+
+    if dicionario[mao_jogador1] == mao_jogador2:
+        return mao_jogador2.capitalize() + ' Ganhou!'
+    else:
+        return mao_jogador1.capitalize() + ' Ganhou!'
+
+@when('C.A jogar "{mao_1}" e C.B jogar "{mao_2}"')
 def jogada_igual(context, mao_1, mao_2):
     context.resultado_jogada = jogo_jokenpo(mao_1, mao_2)
 
 @then('gera empate')
 def assertiva_jogada_igual(context):
     assert context.resultado_jogada == 'Empate'
+
+@then('"{jogada}" ganha')
+def assertiva_jogada_vencedora(context, jogada):
+    assert context.resultado_jogada == f'{jogada} Ganhou!'
